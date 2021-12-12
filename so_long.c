580e4f70200b184a23869cde9f8de495d7407746
Mohammed Brighli <brmohamm@e1r8p10.1337.ma>
2021-12-12 14:19:17 +0100

    20

--- so_long.c
+++ so_long.c
@@ -3,116 +3,87 @@
 int coin_point = 0;
 int player_count = 0;
 
+void move(int win,t_data *data,char v , int y ,int x)
+{
+	int w = 80;
+	int z = -80;
+	player_count ++;
+	printf("%d\n",player_count);
+	if(v =='e' && coin_point == 0)
+		exit(0);
+	if (x == -1 || y == -1)
+	{
+		w *= -1;
+		z *= -1;
+	}
+	if(v == 'c' && coin_point != 0)
+	{
+		data->result[(data->high / 80) + y][(data->whith /80) + x] = '0';
+		if(x == -1 || x == 1)
+		{
+			data->whith = data->whith + w;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
+			data->whith = data->whith + z;
+		}
+		if(y == -1 || y == 1)
+		{
+			data->high =data->high + w;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
+			data->high =data->high + z;
+		}
+		coin_point--;
+	}
+	if(v == '0' || v == 'c' || v == 'p')
+	{
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
+		if(x == -1 || x == 1)
+			data->whith =data->whith + w;
+		if(y == -1 || y == 1)
+			data->high = data->high + w;
+		if(x == -1 )
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->player_left,data->whith, data->high);
+		else
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->player,data->whith, data->high);
+	}
+}
+
 int key(int key, t_data *data)
 {
 	//w=13  s=1  d=2 a=0 
 	//up = 126  right = 124 left = 123 down = 125
 	//esc = 53
 	int win = 0;
-	
 	char d = data->result[(data->high / 80)][(data->whith /80) + 1];
 	char w = data->result[(data->high / 80) - 1][data->whith /80];
 	char a = data->result[data->high / 80][(data->whith /80) - 1];
 	char s = data->result[(data->high / 80) + 1][data->whith /80];
-
 	if (key == 53)
 		exit(0);
-	if(a =='e' && coin_point == 0)
-		exit(0);
-	if(s =='e' && coin_point == 0)
-		exit(0);
-	if(d =='e' && coin_point == 0)
-		exit(0);
-	if(w =='e' && coin_point == 0)
-		exit(0);
-
-
-//////////////////d or right
+////////////////d or right
 	if(win == 0 && (key == 2 || key == 124) && (d == '0' || d == 'p' 
-		|| d == 'c')) 
-	{
-		player_count ++;
-		printf("%d\n",player_count);
-		if(d == 'c' && coin_point != 0)
-		{
-			data->result[(data->high / 80)][(data->whith /80) + 1] = '0';
-			data->whith += 80;
-			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-			data->whith -= 80;
-			coin_point--;
-		}
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-		data->whith += 80;
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->player,data->whith, data->high);
-		
-	}
-
-	/////////////// a or left
+		|| d == 'c' || d == 'e')) 
+		move(win,data,d,0,1);
+/////////////// a or left
 	if(win == 0 && (key == 0 || key == 123) && (a == '0' || a == 'p' 
-		|| a == 'c' )) 
-	{
-		player_count ++;
-		printf("%d\n",player_count);
-		if(a == 'c' && coin_point != 0)
-		{
-			data->result[data->high / 80][(data->whith /80) - 1] = '0';
-			data->whith -= 80;
-			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-			data->whith += 80;
-			coin_point--;
-		}
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-		data->whith -= 80;
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->player_left,data->whith, data->high);
-		
-	}
-
-	/////////// w or up
+		|| a == 'c' || a == 'e')) 
+		move(win,data,a,0,-1);
+/////////////// w or up
 	if(win == 0 && (key == 13 || key == 126) && (w == '0' || w == 'p' 
-		|| w == 'c' )) 
-	{
-		player_count ++;
-		printf("%d\n",player_count);
-		if(w == 'c')
-		{
-			data->result[(data->high / 80) - 1][data->whith /80] = '0';
-			data->high -= 80;
-			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-			data->high += 80;
-			coin_point--;
-		}
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-		data->high -= 80;
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->player,data->whith, data->high);
-	}
-
-	 //////////// s or down
+		|| w == 'c' || w == 'e')) 
+		 move(win,data,w,-1,0);
+/////////////// s or down
 	if(win == 0 && (key == 1 || key == 125) && (s == '0' || s == 'p'
-		|| s == 'c'))
-	{
-		player_count ++;
-		printf("%d\n",player_count);
-		if(s == 'c' && coin_point != 0)
-		{
-			data->result[(data->high / 80) + 1][data->whith /80] = '0';
-			data->high += 80;
-			mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-			data->high -= 80;
-			coin_point--;
-		}
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->plat,data->whith, data->high);
-		data->high += 80;
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->player,data->whith, data->high);
-	}
+		|| s == 'c'|| s == 'e'))
+	 	move(win,data,s,1,0);
 	if(coin_point == 0)
 	{
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->door_open,data->door_whith, data->door_high);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->door_close,data->door_whith, data->door_high);
 		win = 1;
 	}
 	return (0);
 }
 
-static void error_game(t_data data)
+void error_game(t_data data)
 {
 	int i = 0;
 	int len = 0;
@@ -265,7 +236,7 @@ int main(int ac, char **av)
 			if(bufer[fd] == 'e')
 			{
 				door_cont++;
-				mlx_put_image_to_window(data.mlx, data.mlx_win, data.door_close, whith, high);
+				mlx_put_image_to_window(data.mlx, data.mlx_win, data.door_open, whith, high);
 				data.door_high = high;
 				data.door_whith = whith;
 			}
